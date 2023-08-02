b2fe434095c8ba86a66350b25b71739edc4c6e1b
Jay E <60209289+jayjoe101@users.noreply.github.com>
2023-08-01 21:10:09 -0400

    Add files via upload

--- day-26-NATO_alpha/main.py
+++ day-26-NATO_alpha/main.py
@@ -4,8 +4,14 @@ import pandas
 pho_alpha = pandas.read_csv('nato_phonetic_alphabet.csv')
 pho_dict = {row.letter:row.code for (index, row) in pho_alpha.iterrows()}
 
-word = [c.upper() for c in input('Enter a word: ')]
 
-result = [pho_dict[letter] for letter in word]
-print(result)
+while True:
+    try:
+        word = [c.upper() for c in input('Enter a word: ')]
 
+        result = [pho_dict[letter] for letter in word]
+        print(result)
+        if input('Want to generate another word? ') == 'n':
+            break
+    except KeyError:
+        print(f'{word} contains characters outside of the alphabet')
